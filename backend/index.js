007e7bafeca87ff5dcad3fcabbb8f195dc0e5a1a
Snehil <snehilsneh888@gmail.com>
2021-06-23 23:09:10 +0530

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express=require('express');
 const env=require('dotenv');
 const app=express();
+const bodyParser=require('body-parser');
+const mongoose = require("mongoose");
+
+
+//routes
+const userRoutes=require("./src/routes/user");
 
 //env variable
 env.config();
+
+//mongo
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database connected Babyy!"))
+  .catch((err) => console.log(err));
+
+//middlewares
+app.use(express.json());
+app.use('/api',userRoutes);
+
+
+// app.get('/',(req,res,next)=>{
+//     res.status(200).json({
+//         message:"Fuck you"
+//     })
+// })
 
 app.listen(process.env.PORT,()=>{
     console.log('Backend started Boss');
---
+++ backend/src/routes/user.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const router=express.Router();
+
+router.post('/signin',(req,res)=>{
+
+});
+
+
+router.post('/signup',(req,res)=>{
+
+});
+
+
+
+
+module.exports=router;
