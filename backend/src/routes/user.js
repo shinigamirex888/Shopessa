aba38df94280436ac22142298a83624e7604f5df
Snehil <snehilsneh888@gmail.com>
2021-06-23 23:47:53 +0530

    User Schema added

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,12 +1,18 @@
 const express=require('express');
 const router=express.Router();
+const User=require('../models/user');
+
 
 router.post('/signin',(req,res)=>{
+
+
 
 });
 
 
 router.post('/signup',(req,res)=>{
+    
+
 
 });
 
