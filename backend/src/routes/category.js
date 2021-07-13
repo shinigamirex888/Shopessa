ce4c936bf1812968272bbde8e86967230fd0c05e
Snehil <snehilsneh888@gmail.com>
2021-07-13 13:52:36 +0530

    Added jwt authorizaton for Adding categories to admin only and some more common middlewares

--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const slugify = require('slugify');
-const Category=require('../models/category')
+const Category=require('../models/category');
+const {requireSignin,adminMiddleware}=require('../common-middleware/index')
 const {addCategory,getCategories}=require('../controller/category');
 const router=express.Router();
 
 
-router.post('/category/create',addCategory);
+router.post('/category/create',requireSignin,adminMiddleware,addCategory);
 router.get('/category/getcategory',getCategories);
 
 
